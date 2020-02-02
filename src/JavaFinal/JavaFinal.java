c230d5c9a829aaaa9539bb58b98b53155c2d8a0a
Md.Saddam Hossain <csesolve6548@gmail.com>
2020-02-02 19:21:53 +0600

    Java Final

---
+++ src/JavaFinal/JavaFinal.java
@@ -0,0 +1,11 @@
+
+package JavaFinal;
+
+public class JavaFinal {
+    public static void main(String[] args) {
+        
+        final int x = 100;
+        x = 200;//compile time error
+    }
+  
+}
